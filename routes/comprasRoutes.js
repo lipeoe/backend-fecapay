7d4453102c59e61e158c80dec830c92b145a401c
Felipe <95989152+lipeoe@users.noreply.github.com>
2025-05-18 11:27:20 -0300

    metodo de pagamento

--- routes/comprasRoutes.js
+++ routes/comprasRoutes.js
@@ -4,3 +4,5 @@ const router = express.Router()
 const {payment} = require('../controllers/comprasController')
 
 router.post('/pagamento/:ra', payment)
+
+module.exports = router
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const userRoutes = require('./routes/userRoutes')
 const balanceRoutes = require('./routes/balanceRoutes')
 const boletoRoutes = require('./routes/boletosRoutes')
 const produtosRoutes = require('./routes/produtosRoutes')
+const comprasRoutes = require('./routes/comprasRoutes')
 
 
 const PORT = process.env.PORT || 3001
@@ -13,7 +14,7 @@ const PORT = process.env.PORT || 3001
 app.use(express.json())
 app.use(cors())
 
-app.use('/api', userRoutes, balanceRoutes, boletoRoutes, produtosRoutes)
+app.use('/api', userRoutes, balanceRoutes, boletoRoutes, produtosRoutes, comprasRoutes)
 
 
 app.listen(PORT, () =>{
